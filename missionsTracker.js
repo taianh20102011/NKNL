f633fbaa2a282ee5de2d0fa25feec37d1b0134c3
taianh20102011 <phamngoctaianh6@gmail.com>
2025-10-16 22:11:20 +0700

    Update missionsTracker.js

--- missionsTracker.js
+++ missionsTracker.js
@@ -1,23 +1,9 @@
 // missionsTracker.js
 // ✅ Hệ thống theo dõi nhiệm vụ trung tâm (đồng bộ local + Firestore)
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCtp4izpF1GCH2qWpeLtZOdk33A_iNKzqg",
-  authDomain: "nknl-d7b54.firebaseapp.com",
-  projectId: "nknl-d7b54",
-  storageBucket: "nknl-d7b54.firebasestorage.app",
-  messagingSenderId: "792185587281",
-  appId: "1:792185587281:web:585e98f2f87d7d59031a70",
-  measurementId: "G-TC7XHSSCBX"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 const LS_MISSIONS = 'missions_state';
 
 /**
